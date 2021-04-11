7b8a5a3572f7c27503e6baf079efe5f416583670
Lucas Keller <ljkeller@ucdavis.edu>
2021-04-11 10:38:42 -0700

    Review card style + click

--- components/ReviewCard.js
+++ components/ReviewCard.js
@@ -2,39 +2,54 @@ import {
   Box,
   Button,
   Card,
+  CardActionArea,
   CardActions,
   CardContent,
+  makeStyles,
   Typography,
 } from "@material-ui/core";
 import Link from "next/link";
 import React from "react";
 
+const useStyles = makeStyles({
+  card: {
+    boxSizing: "border-box",
+    border: "1px solid white",
+    transition: "border .15s, color .15s",
+    "&:hover": {
+      border: "1px solid #EF8354",
+      color: "#EF8354",
+    },
+  },
+});
+
 export default function ReviewCard({ review }) {
+  const classes = useStyles();
+
   return (
-    <Card key={review._id}>
-      <CardContent>
-        <Typography variant="h6">{review.name}</Typography>
-        <Typography variant="subtitle2" gutterBottom>
-          {review.date}
-        </Typography>
-        <Typography
-          variant="body2"
-          style={{
-            height: 100,
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-          }}
-        >
-          {review.body}
-        </Typography>
-      </CardContent>
-      <CardActions style={{ padding: "0 16px 16px" }}>
-        <Link href={`/reviews/${review._id}`}>
-          <Button color="primary" style={{ textDecoration: "underline" }}>
-            Read More &rarr;
-          </Button>
-        </Link>
-      </CardActions>
-    </Card>
+    <Link href={`/review/${review._id}`}>
+      <Card key={review._id} className={classes.card}>
+        <CardActionArea>
+          <CardContent style={{ paddingBottom: 16 }}>
+            <Typography variant="h6">{review.name}</Typography>
+            <Typography variant="subtitle2" gutterBottom>
+              {review.date}
+            </Typography>
+            <Typography
+              variant="body2"
+              style={{
+                height: 100,
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+              }}
+              gutterBottom
+            >
+              {review.body}
+            </Typography>
+            <Typography variant="button">Read More</Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Link>
   );
 }
