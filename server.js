37c51ebb080ff4d2a10284ddc0feabb69bb38b3a
Lucas <ljkeller@ucdavis.edu>
2021-04-17 14:00:12 -0700

    Localhost on HTTPS

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const { createServer } = require("https");
 const { parse } = require("url");
 const next = require("next");
 const fs = require("fs");
+
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
 const httpsOptions = {
-  key: fs.readFileSync("./certificates/localhost.key"),
-  cert: fs.readFileSync("./certificates/localhost.crt"),
+  key: fs.readFileSync("./certificates/localhost+1-key.pem"),
+  cert: fs.readFileSync("./certificates/localhost+1.pem"),
 };
+
 app.prepare().then(() => {
   createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
