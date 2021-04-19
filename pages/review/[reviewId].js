1f46c233687257b14f31bd2e6e81863d9c2c84d5
Lucas <ljkeller@ucdavis.edu>
2021-04-19 15:33:22 -0700

    Review page improvements

--- pages/review/[reviewId].js
+++ pages/review/[reviewId].js
@@ -1,12 +1,27 @@
 import { Typography } from "@material-ui/core";
+import { Rating } from "@material-ui/lab";
 import React from "react";
 import Layout from "../../components/Layout";
+import appConstants from "../../util/constants";
 
 export default function Home({ review }) {
   return (
     <Layout titleAddition={`${review.name} - Film Review`}>
-      <Typography variant='h5'>{`${review.name} Review`}</Typography>
-      <Typography variant='body2'>{review.body}</Typography>
+      <div style={appConstants.contentArea}>
+        <Typography
+          variant='h5'
+          gutterBottom
+        >{`${review.name} — Film Review`}</Typography>
+        <div style={{ marginBottom: 10 }}>
+          <Typography variant='caption' gutterBottom>
+            {review.date}
+          </Typography>
+        </div>
+        <div style={{ marginBottom: 10 }}>
+          <Rating value={review.rating} precision={0.5} readOnly />
+        </div>
+        <Typography variant='body2'>{review.body}</Typography>
+      </div>
     </Layout>
   );
 }
